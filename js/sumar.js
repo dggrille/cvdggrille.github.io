a3a88609e814d250be96a31e279c04ed401a52e0
Diego Grille <dggrille@sanlus.edu.ar>
2023-03-17 08:43:53 -0300

    Mas arreglo en mini calculadora

--- js/sumar.js
+++ js/sumar.js
@@ -1,6 +1,5 @@
 
-/* No se logra visualizar el resultado en la pagina PERO SI EN LA CONSOLA ¿Por que sera? Me notifica que TypeError no capturado: no se pueden leer las propiedades de nulo (leyendo 'estilo')
-    en HTMLButtonElement.<anónimo> (sumar.js:12:51) style.display = "block";*/
+
     document.getElementById("sumar").addEventListener("click", function() {
     let numeroA = document.getElementById("numero1").value;
     console.log("El valor del numero A es: "+numeroA);
@@ -10,7 +9,7 @@
     console.log("El resultado de la suma es: "+resultado);
 
     document.getElementById("resultado").innerHTML = resultado;
-    document.getElementById("contenedorResultado").style.display = "block";
+    document.getElementById("contenedor_resultado").style.display = "block";
 
 })
 
